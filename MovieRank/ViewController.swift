2113e592e70abd6a4fedf75fd56ba3df7f1dca39
ray <audi3m@naver.com>
2024-06-07 11:41:57 +0900

    Initial Commit

---
+++ MovieRank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieRank
+//
+//  Created by J Oh on 6/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
